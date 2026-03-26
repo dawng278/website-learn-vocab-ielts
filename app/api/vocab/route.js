34b4c7a2bec049402911bf522ce0836ea68b0326
dawng278 <haidang27082005@gmail.com>
2026-03-26 09:08:42 +0700

    Rename lib/mongodb.js to lib/db.js to fix module shadowing bug on Vercel

--- app/api/vocab/route.js
+++ app/api/vocab/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import clientPromise from '../../../lib/mongodb';
+import clientPromise from '../../../lib/db';
 import topicsData from '../../../all_topics.json';
 
 export const dynamic = 'force-dynamic';
